9be45fed2367a933dcc96af927d13d803bc9ad46
Mukhamadali <adakhamovali2009@gmail.com>
2023-09-23 13:23:55 +0300

    frontend

---
+++ frontenVideo/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const cors = require("cors")
+const path = require('path')
+const app = express();
+
+app.use(cors())
+
+const mediaRoutes = require("./routes/media")
+
+const mongodbUrl = "mongodb://127.0.0.1:27017/frontend-uploud"
+
+app.use("/api/vl/media", mediaRoutes)
+app.use("/public", express.static(path.join(__dirname, 'public')))
+
+
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+})
+
+
+mongoose.connection.on("connected", () => {
+  console.log("Server Connected");
+})
+
+mongoose.connection.on("error", (err) => {
+  console.log("Server Error", err);
+})
+
+
+app.listen(5000, () => {
+  console.log("Server Starded on port 5000");
+
+});
